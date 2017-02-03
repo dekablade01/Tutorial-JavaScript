a8a7e0e32613783ef29bca9ddc80fe45c3e5fcec
Issarapong Poesua <dekablade01@gmail.com>
2017-02-03 22:03:47 +0700

    meterToFoot(2.5)

---
+++ app.js
@@ -0,0 +1,11 @@
+// ---- 0.2 Function
+// No 1
+function meterToFoot(m)
+{
+	let oneMeterIsEqualTo = 3.28084;
+	let feet = m*oneMeterIsEqualTo;
+	console.log(feet);
+	return feet;
+}
+
+meterToFoot(2.5);
